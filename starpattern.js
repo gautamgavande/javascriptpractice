1a156c6a0c1344497021bb99bcea53b7a8b87c98
gautamgavande <gautamgavande2436@gmail.com>
2024-07-19 14:08:21 +0530

    add star pattern

---
+++ starpattern.js
@@ -0,0 +1,35 @@
+// let str=""
+// for(let i=0;i<5;i++){
+//     for(let j=i;j<=i;j++){
+//         str +="*"
+//     }
+//     console.log(str)
+// }
+// let str=""
+// let n=5
+// for(let i=0;i<n;i++){
+//     for(let j=0;j<=n-i;j++){
+//         str+="*"
+//     }
+//     console.log(str)
+//     }
+//     let rows=5
+//     var count=1;
+//   for (let i = rows; i > 0; i--) {
+//     let stars = '';
+//     for (let j = 0; j < i; j++) {
+//      stars = (stars+ "    " +count++)
+//     }
+//     console.log(stars);
+//   }
+
+// for(let i=0;i<5;i++){
+//     console.log("%")
+// }
+
+
+// let symbols = '';
+// for(let i = 0; i < 5; i++){
+//     symbols += '%';
+// }
+// console.log(symbols);
